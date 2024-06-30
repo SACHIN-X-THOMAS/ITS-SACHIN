391e48f393597577a9750320c4032165dabfd56e
SACHIN-X-THOMAS <161201708+SACHIN-X-THOMAS@users.noreply.github.com>
2024-06-30 08:36:46 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BWFTIME/AshishMusic",
+    "https://github.com/SACHIN-X-THOMAS/SACHIN-REPO",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Bwf")
 GIT_TOKEN = getenv(
